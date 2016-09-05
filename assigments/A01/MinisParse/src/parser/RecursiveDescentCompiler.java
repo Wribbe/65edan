1953853eecc55d7d691f37530688104dc19c55af
Stefan Eng <eng_steff@hotmail.com>
2016-09-05 17:55:31 +0200

    Fleshing out the recursive descent parser.

--- assigments/A01/MinisParse/src/parser/RecursiveDescentCompiler.java
+++ assigments/A01/MinisParse/src/parser/RecursiveDescentCompiler.java
@@ -1,16 +1,53 @@
 package lang;
 
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import java.io.FileNotFoundException;
+
 import lang.ast.LangParser;
 import lang.ast.LangScanner;
+
+
 
 import static lang.ast.LangParser.Terminals.*;
 
 public class RecursiveDescentCompiler extends RDPTemplate {
 
+    private static RecursiveDescentCompiler compiler;
+    private static LangScanner scanner;
+
+    public static void fail(String message) {
+        System.err.println(message);
+        System.exit(0);
+    }
+
     public void parseProgram() {
+//        compiler.accept();
+//        System.out.println(compiler.peek());
+//        System.out.println("MUPP");
     }
 
     public static final void main(String[] args) {
-        System.out.println("HELLO WOLRD!");
+
+        FileInputStream stream = null;
+        try {
+            stream = new FileInputStream(args[0]);
+        } catch (FileNotFoundException e) {
+            fail("File: \"" + args[0] + "\" not found, aborting.");
+        }
+
+        Reader reader = new InputStreamReader(stream);
+
+        scanner = new LangScanner(reader);
+        compiler = new RecursiveDescentCompiler();
+        compiler.parse(scanner);
+        //try {
+        //} catch () {
+
+
+//        Reader r = new InputStreamReader();
+//        LangScanner scanner = new LangScanner(r);
     }
 }
