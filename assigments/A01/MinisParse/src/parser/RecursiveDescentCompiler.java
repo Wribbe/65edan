59623881f715ba6d9ea3498071e149e21fdb928c
Stefan Eng <eng_steff@hotmail.com>
2016-09-06 06:56:22 +0200

    Parser working for shortest program.

--- assigments/A01/MinisParse/src/parser/RecursiveDescentCompiler.java
+++ assigments/A01/MinisParse/src/parser/RecursiveDescentCompiler.java
@@ -9,8 +9,6 @@ import java.io.FileNotFoundException;
 import lang.ast.LangParser;
 import lang.ast.LangScanner;
 
-
-
 import static lang.ast.LangParser.Terminals.*;
 
 public class RecursiveDescentCompiler extends RDPTemplate {
@@ -24,9 +22,37 @@ public class RecursiveDescentCompiler extends RDPTemplate {
     }
 
     public void parseProgram() {
-//        compiler.accept();
-//        System.out.println(compiler.peek());
-//        System.out.println("MUPP");
+        switch(compiler.peek()) {
+//            case FOR: forStatement(); break;
+//            case IF: ifStatement(); break;
+            case ID: assignment(); break;
+            default: compiler.error("Expected statement, got " +
+                                    compiler.peek());
+        }
+    }
+
+    private static void forStatement() {
+//        compiler.accept(FOR);
+//        compiler.accept(ID);
+//        compiler.accept(ASSIGN);
+//        expression();
+//        compiler.accept(DO);
+//        statement();
+//        compiler.accept(OD);
+    }
+
+    private static void expression() {
+        switch(compiler.peek()) {
+            case ID: compiler.accept(ID); break;
+            case NUMERAL: compiler.accept(NUMERAL); break;
+            case NOT: compiler.accept(NOT); expression(); break;
+        }
+    }
+
+    private static void assignment() {
+        compiler.accept(ID);
+        compiler.accept(ASSIGN);
+        expression();
     }
 
     public static final void main(String[] args) {
@@ -43,11 +69,6 @@ public class RecursiveDescentCompiler extends RDPTemplate {
         scanner = new LangScanner(reader);
         compiler = new RecursiveDescentCompiler();
         compiler.parse(scanner);
-        //try {
-        //} catch () {
-
-
-//        Reader r = new InputStreamReader();
-//        LangScanner scanner = new LangScanner(r);
+        System.out.println("Valid syntax.");
     }
 }
