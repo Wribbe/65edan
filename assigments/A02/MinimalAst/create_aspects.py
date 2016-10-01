67e3b162cab0332132610020ad330798403165f1
wribbe <eng_steff@hotmail.com>
2016-10-01 16:02:50 +0200

    create_aspects.py script extended to create TraversingVisitor class.

--- assigments/A02/MinimalAst/create_aspects.py
+++ assigments/A02/MinimalAst/create_aspects.py
@@ -1,5 +1,5 @@
 
-def create_structure(aspect_name, list_objects, aspect_methods):
+def create_aspect(aspect_name, list_objects, aspect_methods):
 
     lines = []
 
@@ -15,6 +15,7 @@ def create_structure(aspect_name, list_objects, aspect_methods):
     defult_visit_method = "return visitor.visit(this, data);"
 
     accept_objects = ["ASTNode"] + list_objects
+
     for element in accept_objects:
         format_string = "public Object {}.accept({} {}, Object data) {{"
         asp_lower = aspect_name.lower()
@@ -23,6 +24,58 @@ def create_structure(aspect_name, list_objects, aspect_methods):
         element_method = aspect_methods.get(element)
         if not element_method:
             element_method = defult_visit_method
+        lines.append(element_method)
+
+        lines.append("}")
+
+    lines.append("}")
+
+    return lines
+
+
+def create_class(class_name, implementations, list_objects, methods_objects):
+
+    lines = [
+             'package lang;',
+             'import lang.ast.*;',
+            ]
+
+    decleration_tokens = [
+                          'public',
+                          'abstract',
+                          'class',
+                           class_name,
+                          'implements',
+                          ' '.join(implementations),
+                          '{'
+                         ]
+
+    class_declaration = ' '.join(decleration_tokens)
+
+    lines.append(class_declaration)
+
+
+    private_method_lines = [
+            "private Object visitChildren(ASTNode node, Object data) {",
+            "for (int i = 0; i < node.getNumChild(); ++i) {",
+            "node.getChild(i).accept(this, data);",
+            "}",
+            "return data;",
+            "}",
+        ]
+
+    lines += private_method_lines
+
+    default_class_method = "return visitChildren(node, data);"
+
+    for element in list_objects:
+
+        format_string = "public Object visit({} node, Object data) {{"
+        lines.append(format_string.format(element))
+
+        element_method = methods_objects.get(element)
+        if not element_method:
+            element_method = default_class_method
         lines.append(element_method)
 
         lines.append("}")
@@ -88,11 +141,18 @@ def main(args=[]):
                            ' getClass().getName());'
             }
 
-    unindented_lines = create_structure("Visitor", objects, aspect_methods)
+    unindented_lines = create_aspect("Visitor", objects, aspect_methods)
     indented_lines = indent(unindented_lines)
 
     print("\n".join(indented_lines))
 
+    unindented_class_lines = create_class("TraversingVisitor",
+                                          ['lang.ast.Visitor'],
+                                          objects,
+                                          {})
+
+    class_lines = indent(unindented_class_lines)
+    print('\n'.join(class_lines))
 
 if __name__ == "__main__":
     main()
