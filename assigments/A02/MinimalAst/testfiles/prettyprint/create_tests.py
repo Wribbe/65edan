e7912af213eb455446da6d25b053f9335cf80405
wribbe <eng_steff@hotmail.com>
2016-10-11 08:36:10 +0200

    Update create_tests.py to handle custom result.

--- assigments/A02/MinimalAst/testfiles/prettyprint/create_tests.py
+++ assigments/A02/MinimalAst/testfiles/prettyprint/create_tests.py
@@ -5,37 +5,62 @@ import re
 
 TEST_FILE = "tests.txt"
 
+class SourceResult:
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.source = ""
+        self.result = ""
+
 def main():
 
-    temp_buffer = []
-    file_data = {}
-    lines = [line.strip('\n') for line in open(TEST_FILE).readlines()]
+    with open(TEST_FILE, 'r') as source_handle:
+        lines = [line.strip('\n') for line in source_handle.readlines()]
 
-    filename = ""
+    line_buffer = []
+    line_segments = []
     for line in lines:
-        if line.startswith('--'):
-            if filename: # Don't do the first time.
-                file_data[filename] = '\n'.join(temp_buffer)
-                temp_buffer = []
-            filename = re.sub('--\s*', '', line)
+        if line.startswith('--') and line_buffer:
+            line_segments.append(line_buffer)
+            line_buffer = []
+            line_buffer.append(line)
             continue
+        line_buffer.append(line)
+
+    objects = []
+    for segment in line_segments:
+        ans_part = False
+        line_buffer = []
+        filename = re.sub('---*\s*', '', segment[0])
+        pair_object = SourceResult(filename)
+        for line in segment[1:]:
+            if line.startswith('=='):
+                ans_part = True
+                pair_object.source = '\n'.join(line_buffer)
+                line_buffer = []
+                continue
+            line_buffer.append(line)
+
+        if ans_part:
+            pair_object.result = '\n'.join(line_buffer)
         else:
-            temp_buffer.append(line)
+            pair_object.source = '\n'.join(line_buffer)
 
+        objects.append(pair_object)
 
-    if temp_buffer:
-        file_data[filename] = '\n'.join(temp_buffer)
-        temp_buffer = []
-
-
-    fmt_lang = "{}.lang"
     fmt_expected = "{}.expected"
+    fmt_lang = "{}.lang"
 
-    for base_name, test_data in file_data.items():
-        with open(fmt_lang.format(base_name), 'w') as file_handle:
-            file_handle.write(test_data+'\n')
-        with open(fmt_expected.format(base_name), 'w') as file_handle:
-            file_handle.write(test_data+'\n')
+    for object in objects:
+        filename = object.filename
+        with open(fmt_lang.format(filename), 'w') as source_handle:
+            with open(fmt_expected.format(filename), 'w') as result_handle:
+                source_handle.write(object.source)
+                if not object.result:
+                    result_handle.write(object.source)
+                else:
+                    result_handle.write(object.result)
+
 
 if __name__ == "__main__":
     main()
